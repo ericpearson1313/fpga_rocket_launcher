b725ad47e5e1920cc0b02abea7652d220d625372
Eric Pearson <ericp@untether.ai>
2024-05-27 15:48:21 -0400

    Initial operation, basic bang-bang controller

---
+++ blaster.sv
@@ -0,0 +1,302 @@
+`timescale 1ns / 1ps
+module blaster
+
+#(
+	// Parameter Declarations
+	parameter UNIQ_ID = 32'h0000_0000
+)
+
+(
+	// Input Buttons
+	input  logic arm_button,
+	input  logic fire_button,
+
+	// Output LED/SPK
+	output logic arm_led,
+	output logic cont_led,
+	output logic speaker,
+	
+	// Charger
+	input  logic lt3420_done,
+	output logic lt3420_charge,
+
+	// Voltage Controls
+	output logic pwm,
+	output logic dump,
+
+	// Continuity feedback
+	input  logic cont,
+	
+	// External A/D Converters (2.5v)
+	output logic        ad_cs,
+	input  logic  [1:0] ad_sdata_a,
+	input  logic  [1:0] ad_sdata_b,
+
+	// External Current Control Input
+	input	 logic  [2:0] iset, // Current target in unit amps  
+	
+	// Input clock, reset
+	input logic clk,
+	input logic reset
+	
+);
+
+	// Module Item(s)
+
+
+// System State machine
+
+parameter S_IDLE   		= 0;
+parameter S_CHARGE  		= 1;
+parameter S_READY   		= 2;
+parameter S_FIRE    		= 3;
+parameter S_DISCHARGE  	= 4;
+parameter S_OCP    		= 5;
+
+
+
+logic fire_debounce;
+logic igniter_burnout;
+logic cap_over_current;
+logic cap_under_voltage;
+logic 		 adc_valid;
+
+
+logic [3:0] state_q;
+
+always @(posedge clk) begin : _launch_sm
+	if( reset ) begin
+		state_q <= S_IDLE;
+	end else begin
+		case( state_q ) 
+		S_IDLE : 
+			begin
+				state_q <= S_CHARGE; // Advance to charge state 
+			end
+		S_CHARGE :
+			begin
+				if( lt3420_done ) begin
+					state_q <= S_READY;
+				end else begin
+					state_q <= S_CHARGE;
+				end
+			end
+		S_READY :
+			begin
+				if( fire_button ) begin
+					state_q <= S_FIRE;
+				end else begin
+					state_q <= S_READY;
+				end
+			end
+		S_FIRE :
+			begin
+				if( fire_debounce && !fire_button ) begin
+					state_q <= S_DISCHARGE;
+				end else if( adc_valid && igniter_burnout ) begin
+					state_q <= S_DISCHARGE; // igniter burned out
+				end else if( adc_valid && cap_over_current ) begin
+					state_q <= S_OCP; // over current
+				end else if( adc_valid && cap_under_voltage ) begin
+					state_q <= S_DISCHARGE; // cap is empty
+				end else begin
+					state_q <= S_FIRE;
+				end
+			end
+		S_DISCHARGE :
+			begin
+				state_q <= S_DISCHARGE; // terminal state
+			end
+		S_OCP :
+			begin
+				state_q <= S_OCP; // terminal state
+			end
+		default :
+			begin
+				state_q <= S_IDLE;
+			end
+		endcase
+	end
+end
+
+// Debounce, 10ms  fire button release.
+// Fire button depressed is acted on immediately.
+// After debounce period the first rire button release will terminate the fire sequence. 
+
+parameter DEBOUNCE = 22'h00_0040; // 22'h3F_FFFF ; // 48Mhz / 10ms = 4.8 million cycles. Just use 22 bit countdown;
+
+reg [21:0] debounce_div;
+always @(posedge clk) begin
+	if( reset ) begin
+		debounce_div <= DEBOUNCE;
+	end else if( state_q == S_FIRE ) begin
+		debounce_div <= ( debounce_div == 22'd0 ) ? 22'd0 : debounce_div - 22'd1;
+	end else begin
+		debounce_div <= debounce_div;
+	end
+end
+ 
+assign fire_debounce = ( debounce_div == 2'd0 ) ? 1'b1 : 1'b0;
+
+// Capcitor Charger Control
+
+assign lt3420_charge = (state_q == S_CHARGE || state_q == S_READY ) ? 1'b1 : 1'b0;
+
+
+// Just use 24 bit counter, 16m cycles, use MSB for 3 Hz
+
+reg [24:0] blink_div;
+always @(posedge clk) begin
+	if( reset ) begin
+		blink_div <= 24'd0;
+	end else begin
+		blink_div <= blink_div + 24'd1;
+	end
+end
+
+logic blink;
+assign blink = blink_div[23];
+
+// ARM Led
+
+assign arm_led = (state_q == S_FIRE   ) ? 1'b1 :
+					  (state_q == S_READY  ) ? 1'b1 : 
+					  (state_q == S_CHARGE ) ? blink : 1'b0;
+
+// Continuity, pulled low if an igniter present
+
+assign cont_led = (cont) ? blink : 1'b1;
+
+// ADC sample pulse
+
+parameter ADC_CYCLES = 5'd16; // 16 for 48Mhz, 15 for 45Mhz to give a 3Mhz sample rate. 
+										// CS is active low for 14 cycles to give a 12 bit output
+
+reg [4:0] sample_div;
+always @(posedge clk) begin
+	if( reset ) begin
+		sample_div <= ADC_CYCLES - 5'd1;
+	end else if( state_q == S_FIRE ) begin
+		sample_div <= (sample_div == 0) ? (ADC_CYCLES - 5'd1) : sample_div - 5'd1;
+	end else begin
+		sample_div <= sample_div;
+	end
+end
+
+assign ad_cs = ( state_q == S_FIRE && sample_div == 5'd0 ) ? 1'b1 : 1'b0;
+
+// CS pipeline to trigger everything
+logic [20:0] cs_delay;
+always @(posedge clk) begin
+	if( reset ) begin
+		cs_delay[20:0]     <= 21'd0;
+   end else begin
+		// shift chain for the chip select
+		cs_delay[20:0]  <= { cs_delay[19:0], ad_cs };		
+	end
+end
+
+// DATA Input Receiver
+
+logic [11:0] ad_load_a0, ad_load_a1, ad_load_b0, ad_load_b1;
+logic [11:0] ad_hold_a0, ad_hold_a1, ad_hold_b0, ad_hold_b1;
+
+logic [11:0] load;
+
+parameter LOAD_SEL = 0;   // select first load delay, load reg input (ie 1 cycle early).
+parameter HOLD_SEL = 13;  // select output hold delay bit
+parameter VALID_SEL = 14;   // the cycle the adc hold registers are updatead
+
+always @(posedge clk) begin
+	if( reset ) begin
+		ad_load_a0[11:0] <= 12'd0;
+		ad_load_a1[11:0] <= 12'd0;
+		ad_load_b0[11:0] <= 12'd0;
+		ad_load_b1[11:0] <= 12'd0;
+		ad_hold_a0[11:0] <= 12'd0;
+		ad_hold_a1[11:0] <= 12'd0;
+		ad_hold_b0[11:0] <= 12'd0;
+		ad_hold_b1[11:0] <= 12'd0;
+   end else begin
+		// Load Pulse Chain
+		load[11:0] <= { cs_delay[LOAD_SEL], load[11:1] };
+		// low power reg load with bit 
+		for( int ii = 0; ii < 12; ii++ ) begin
+			ad_load_a0[ii] <= ( load[ii] ) ? ad_sdata_a[0] : ad_load_a0[ii];
+			ad_load_a1[ii] <= ( load[ii] ) ? ad_sdata_a[1] : ad_load_a1[ii];
+			ad_load_b0[ii] <= ( load[ii] ) ? ad_sdata_b[0] : ad_load_b0[ii];
+			ad_load_b1[ii] <= ( load[ii] ) ? ad_sdata_b[1] : ad_load_b1[ii];
+		end
+		// Load hold reg 
+		begin
+			ad_hold_a0 <= (cs_delay[HOLD_SEL]) ? ad_load_a0 : ad_hold_a0;
+			ad_hold_a1 <= (cs_delay[HOLD_SEL]) ? ad_load_a1 : ad_hold_a1;
+			ad_hold_b0 <= (cs_delay[HOLD_SEL]) ? ad_load_b0 : ad_hold_b0;
+			ad_hold_b1 <= (cs_delay[HOLD_SEL]) ? ad_load_b1 : ad_hold_b1;
+		end
+	end
+end
+
+assign adc_valid = cs_delay[VALID_SEL];
+
+// ADC Mapping
+
+logic [11:0] vcap, icap, vout, iout;
+
+assign vcap[11:0] = ad_hold_b1;	// F9.2 0.0 to 511.75 volts (1/4 volt)
+assign icap[11:0] = ad_hold_b0;	// F4.8 0.0 to 15.992 amps (1/256 amp)
+assign vout[11:0] = ad_hold_a1;	// F9.2 0.0 to 511.75 volts
+assign iout[11:0] = ad_hold_a0;	// F4.8 0.0 to 15.992 amps
+                                               
+// PWM Motor Control
+// Very Simple initially +/-12.5% margin
+
+logic [11:0] lower_margin, upper_margin;
+
+assign lower_margin = {1'b0, iset[2:0], 8'b0000_0000} - {4'b0000, iset[2:0], 5'b0_0000 };
+assign upper_margin = {1'b0, iset[2:0], 8'b0000_0000} + {4'b0000, iset[2:0], 5'b0_0000 };
+
+always @(posedge clk) begin
+	if( reset ) begin
+		pwm  <= 0;
+	end else begin
+		if( state_q != S_FIRE ) begin
+			pwm <= 1'b0;
+		end else if( !adc_valid ) begin
+			pwm <= pwm;
+		end else if( pwm == 0 && iout <= lower_margin ) begin
+			pwm <= 1'b1;
+		end else if( pwm == 1 && iout >= upper_margin ) begin //  pwm == 1
+			pwm <= 1'b0;
+		end else begin
+			pwm <= pwm;
+		end
+	end
+end
+
+// Burn Through Detect
+// Output with no current and rasied voltage indicates igniter burnt through.
+
+parameter BURNOUT_VOLTAGE = 200 * 4;	// 200 V
+parameter BURNOUT_CURRENT = 256 / 20;  // 50mA  
+parameter CAP_CURRENT_LIMIT = 15 * 256; // 15 Amp limit
+parameter CAP_VOLTAGE_LIMIT = 12 * 4; 	// 12 Volts
+
+assign igniter_burnout = ((vout >= BURNOUT_VOLTAGE) && (iout <= BURNOUT_CURRENT) ) ? 1'b1 : 1'b0;  
+
+assign cap_over_current = ( icap >= CAP_CURRENT_LIMIT ) ? 1'b1 : 1'b0;
+
+assign cap_under_voltage = ( vcap < CAP_VOLTAGE_LIMIT ) ? 1'b1 : 1'b0;
+
+
+
+// Self Discharge. If still powered and firing is done assert turn off 
+
+assign dump = (state_q == S_DISCHARGE) ? 1'b1 : 1'b0;
+
+// Audio Output
+
+assign speaker = 1'b0;
+
+
+endmodule
---
+++ testbench.sv
@@ -0,0 +1,215 @@
+// top level launcher sim.
+`timescale 1ns / 1ps
+module testbench( );
+
+parameter R = 10.0; // Load resistance
+parameter CAP_VOLTAGE = 320.0;
+parameter COIL_UH = 399.0;
+parameter FREQ_MHZ = 48;
+parameter PERIOD_NS = 20.8;
+parameter CAP_UF = 10.0;  // typicall 100uF
+parameter CURRENT = 2 	; // integer amps, 1 to 7
+
+logic arm, fire;
+logic pwm, dump;
+logic done, charge;
+logic n_sclk, n_cs;
+logic [3:0] data, data_n;
+
+    //////////////////////
+    // Let there be Clock!
+    //////////////////////
+    
+    logic clk;
+    initial begin
+        clk = 1'b1;
+        forever begin
+            #(20.8ns) clk = ~clk;
+        end 
+    end
+	 
+	 // Reset generation
+	 
+	 logic reset; // active high
+	 initial begin
+			reset = 1'b1;
+			for( int ii = 0; ii < 3; ii++ ) begin
+				@(posedge clk);
+			end
+			@(negedge clk);
+			reset = 1'b0;
+	 end
+			
+	 // Simulation stop.
+	 
+	 initial begin
+        for( int ii = 0; ii < 10000; ii++ ) 
+				@(posedge clk);
+        $stop;
+	 end
+	 
+
+blaster _uut (
+	// Input Buttons
+	.arm_button( arm ), // arm is the power button
+	.fire_button( fire ),
+
+	// Output LED/SPK
+	.arm_led(),
+	.cont_led(),
+	.speaker(),
+	
+	// Charger
+	.lt3420_done( done ),
+	.lt3420_charge( charge ),
+
+	// Voltage Controls
+	.pwm( pwm ),
+	.dump( dump ),
+
+	// Continuity feedback
+	.cont( 1'b1 ),
+	
+	// Current setting
+	.iset( CURRENT  ),
+	
+	// External A/D Converters (2.5v)
+	.ad_cs( n_cs ),
+	.ad_sdata_a( data[1:0] ),
+	.ad_sdata_b( data[3:2] ),
+
+	// Input clock
+	.clk( clk ),
+	.reset( reset )
+);
+
+
+		
+		// TODO, turn these back to reals (but after I get git setup)
+		
+		real ecap, ecap_n; 		
+		real icap, icap_n; 
+		real vcap, vcap_n; 
+		real iout, iout_n; 
+		real vout, vout_n; 
+	 
+	
+		
+	initial begin
+		// hardware will wait for fire;
+		fire = 1'b0;
+		arm  = 1'b1; // arm assume to go high with power-on at this time.
+		done = 1'b1; // for sim show charge complete
+      // startup delay (for future reset)
+      for( int ii = 0; ii < 10; ii++ ) @(posedge clk); // 10 cycles
+		// Fire
+		fire = 1'b1;
+	end // fire		
+		
+	initial begin
+
+
+	/////////////////////
+	// System Model     
+	/////////////////////	
+	// Model of power module 
+	// root states are capacitor energy and coil (output) current.
+	// The model updates these dynamically from the pwm state
+	// The cap voltage is derived from cap energy, and cap current from pwm and coil current.
+	// The output voltage is a function of igniter resistance and coil (output) current.
+	// 
+	
+		vcap = CAP_VOLTAGE;
+		ecap = (0.5 * CAP_UF * CAP_VOLTAGE * CAP_VOLTAGE)/1000000.0; 
+		icap = 0;
+		vout = 0;
+		iout = 0;
+
+		// on a cycle loop, now responde to PWM 
+		// TODO add dump modelling
+		
+		forever begin
+			@(posedge clk ) ;
+			if( pwm == 0 ) begin
+				// output off, I falls at rate Vout/L*t
+				iout_n = iout - ((vout / (COIL_UH * FREQ_MHZ))); 
+				// The updated Vout is IR
+				vout_n = (iout_n * R);
+				// Icap = 0; Ecap, Vcap unchanged
+				icap_n = 0;
+				vcap_n = vcap;
+				ecap_n = ecap;
+			end else begin 
+				// output is on, I rises by (Vin-Vout)/L
+				iout_n = iout + (((vcap - vout) / (COIL_UH * FREQ_MHZ)));
+				// The updated Vout is IR
+				vout_n = (iout_n * R);
+				// Icap = Iout
+				icap_n = iout_n;
+				// cap drops by energy used this cycle IVT
+				ecap_n = ecap - ((((((iout+iout_n)/2)*vcap) / (FREQ_MHZ*1000000.0)))) ;
+				// votlage is calc from energy
+
+				vcap_n = ($sqrt( (2.0 * ecap_n) / CAP_UF ) * 1000.0);
+
+			end
+			
+			iout = iout_n;
+			vout = vout_n;
+			ecap = ecap_n;
+			vcap = vcap_n;
+			icap = icap_n;		
+		end // Analog model
+	end // analog model
+
+	
+	/////////////////////
+	// AD7352 Model     
+	/////////////////////
+	// Models amplification
+	// adc sampling, conversion
+	// and transmission
+	
+	// ADC sampling and transmission.
+	logic [11:0] sh_vcap;	// 0 to 350 volts  350/4096
+	logic [11:0] sh_icap;	// 0 to 12 amp     12/4096
+	logic [11:0] sh_vout;	// 0 to 350 volts
+	logic [11:0] sh_iout;	// 0 to 12 amp
+
+	assign n_sclk = clk; // same clock, adc just uses negedge.
+	
+	initial begin
+		data_n = 4'b0;
+	   sh_vcap = 0.0;
+		sh_icap = 0.0;
+		sh_vout = 0.0;
+		sh_iout = 0.0;
+		forever begin
+			@(negedge n_sclk ) 
+			while( n_cs != 1 ) begin // it has to start high
+				@(negedge n_sclk ); 
+			end
+			while( n_cs != 0 ) begin // wait for it to go low
+				@(negedge n_sclk ); 
+			end
+			// first falling edge with n_cs active low, output 2nd zero
+			data_n = 4'b0;
+			// sample 
+			sh_vcap[11:0] = int'(vcap * 8  );
+			sh_icap[11:0] = int'(icap * 256);
+			sh_vout[11:0] = int'(vout * 8  );
+			sh_iout[11:0] = int'(iout * 256);
+
+			@(negedge n_sclk ); 
+			for( int bitpos = 11; bitpos >= 0; bitpos-- ) begin
+				data_n[3:0] = { sh_vcap[bitpos], sh_icap[bitpos], sh_vout[bitpos], sh_iout[bitpos] };
+				@(negedge n_sclk ); 
+			end
+			data_n[3:0] = 4'b0;
+		end // adc
+	end
+	
+	// adc oe.
+	assign data[3:0] = ( n_cs == 1'b0 ) ? data_n[3:0] : 4'bxxxx;
+
+endmodule
