4b2d2c08a86b01331ae3dbc0c888039ae03d7c22
Eric Pearson <ericp@untether.ai>
2025-05-24 10:22:55 -0400

    Split out forge launcher wrapper and changed to .sv and fixed some error. cleanish syntax in  Forge and Quartus

--- forge_launcher/ffpga/src/forge_launcher.sv
+++ forge_launcher/ffpga/src/forge_launcher.sv
@@ -1,89 +1,4 @@
 `timescale 1ns / 1ps
-// Top level Chip Wrapper
-(* top *) module forge_launcher_wrapper
-(
-// Forge FPGA built in clk reset
-(* iopad_external_pin, clkbuf_inhibit *) input logic clk,
-(* iopad_external_pin *) input logic resetn,
-
-
-	// Input Buttons
-(* iopad_external_pin *)	input  logic arm_button,
-(* iopad_external_pin *)	input  logic fire_button,
-
-	// Output LED/SPK
-(* iopad_external_pin *)	output logic arm_led_n,
-(* iopad_external_pin *)	output logic cont_led_n,
-(* iopad_external_pin *)	output logic speaker,
-
-	// High Voltage 
-(* iopad_external_pin *)	output logic lt3420_charge,
-(* iopad_external_pin *)	input  logic lt3420_done,
-(* iopad_external_pin *)	output logic pwm,	
-(* iopad_external_pin *)	output logic dump,
-	
-	// External A/D Converters (2.5v)
-(* iopad_external_pin *)	output logic        ad_cs,
-(* iopad_external_pin *)	output logic		ad_sclk,
-(* iopad_external_pin *)	input  logic  [1:0] ad_sdata_a,
-(* iopad_external_pin *)	input  logic  [1:0] ad_sdata_b,
-	
-	// Forge PLL control
-(* iopad_external_pin *) output pll_en,
-(* iopad_external_pin *) output [5:0] pll_refdiv,
-(* iopad_external_pin *) output [11:0] pll_fbdiv,
-(* iopad_external_pin *) output [2:0] pll_postdiv1,
-(* iopad_external_pin *) output [2:0] pll_postdiv2,
-(* iopad_external_pin *) output pll_bypass,
-(* iopad_external_pin *) output pll_clk_selection
-
-);
-
-    // PLL Control, 48 Mhz
-    assign pll_en = 1'b1;
-    assign pll_refdiv = 6'b00_0001;		// Equivalent value in decimal form 6'd1,
-    assign pll_fbdiv = 12'b0000_0001_1000;	// Equivalent value in decimal form 12'd24,
-    assign pll_postdiv1 = 3'b101;		// Equivalent value in decimal form 3'd5,
-    assign pll_postdiv2 = 3'b101;		// Equivalent value in decimal form 3'd5,
-    assign pll_bypass = 1'b0;
-    assign pll_clk_selection = 1'b0;
-
-	// Synchronize reset
-	logic reset;
-	logic [3:0] reset_shift = 4'b0; // initial value upon config is reset
-	always_ff @(posedge clk)
-		{ reset, reset_shift[3:0] } <= { !reset_shift[3], reset_shift[2:0], resetn };
-
-	// create ADC inverted clk for output
-	assign ad_sclk  = !clk;
-
-	forge_launcher _chip (
-		// System
-		.clk			( clk ),
-		.reset			( reset ),
-		// Front Panel
-		.fire_button 	( fire_button ),
-		.arm_button 	( arm_button ),
-		.arm_led_n 		( arm_led_n ),
-		.cont_led_n 	( cont_led_n ),
-		.speaker 		( speaker ),
-		// High Voltage
-		.lt3420_charge 	( lt3420_charge ),
-		.lt3420_done   	( lt3420_done   ),
-		.pwm           	( pwm ),
-		.dump			( dump ),
-		// ADC interface
-		.ad_cs			( ad_cs ),
-		.ad_sdata_a 	( ad_sdata_a ),
-		.ad_sdata_b 	( ad_sdata_b ),
-		// Tie off Debug inputs
-		.iset			( 3'b111 ),
-		.key			( 5'b00000 )
-    );
-endmodule // forge_launcher_wrapper 
-		
-		
-		
 // ForgeFPGA model rocket launcher
 // Digital PWM, current controlled buck converter. capacitive discharge
 // igniter pulse generator
@@ -370,11 +285,6 @@ assign icap = ( ad_b0[11] || ad_b0[10:4] == 7'h7F ) ? 11'b0 : ( ad_b0[10:0] ^ 11
 
 // Modelling Coil Current
 // estimate is before sample and 16x finer timing
-	parameter ADC_VOLTS_PER_DN = 0.2005;
-	parameter ADC_DN_PER_AMP = 205;
-	// Physical parameters
-	parameter CLOCK_FREQ_MHZ = 48;
-	parameter COIL_IND_UH = 390;
 forge_model_coil #( ADC_VOLTS_PER_DN, ADC_DN_PER_AMP, CLOCK_FREQ_MHZ, COIL_IND_UH ) _model (
 	// Input clock
 	.clk( clk ),
---
+++ forge_launcher/ffpga/src/forge_launcher_wrapper.v
@@ -0,0 +1,85 @@
+// Top level Chip Wrapper
+(* top *) module forge_launcher_wrapper
+(
+// Forge FPGA built in clk reset
+(* iopad_external_pin, clkbuf_inhibit *) input logic clk,
+(* iopad_external_pin *) input logic resetn,
+
+
+	// Input Buttons
+(* iopad_external_pin *)	input  logic arm_button,
+(* iopad_external_pin *)	input  logic fire_button,
+
+	// Output LED/SPK
+(* iopad_external_pin *)	output logic arm_led_n,
+(* iopad_external_pin *)	output logic cont_led_n,
+(* iopad_external_pin *)	output logic speaker,
+
+	// High Voltage 
+(* iopad_external_pin *)	output logic lt3420_charge,
+(* iopad_external_pin *)	input  logic lt3420_done,
+(* iopad_external_pin *)	output logic pwm,	
+(* iopad_external_pin *)	output logic dump,
+	
+	// External A/D Converters (2.5v)
+(* iopad_external_pin *)	output logic        ad_cs,
+(* iopad_external_pin *)	output logic		ad_sclk,
+(* iopad_external_pin *)	input  logic  [1:0] ad_sdata_a,
+(* iopad_external_pin *)	input  logic  [1:0] ad_sdata_b,
+	
+	// Forge PLL control
+(* iopad_external_pin *) output pll_en,
+(* iopad_external_pin *) output [5:0] pll_refdiv,
+(* iopad_external_pin *) output [11:0] pll_fbdiv,
+(* iopad_external_pin *) output [2:0] pll_postdiv1,
+(* iopad_external_pin *) output [2:0] pll_postdiv2,
+(* iopad_external_pin *) output pll_bypass,
+(* iopad_external_pin *) output pll_clk_selection
+
+);
+
+    // PLL Control, 48 Mhz
+    assign pll_en = 1'b1;
+    assign pll_refdiv = 6'b00_0001;		// Equivalent value in decimal form 6'd1,
+    assign pll_fbdiv = 12'b0000_0001_1000;	// Equivalent value in decimal form 12'd24,
+    assign pll_postdiv1 = 3'b101;		// Equivalent value in decimal form 3'd5,
+    assign pll_postdiv2 = 3'b101;		// Equivalent value in decimal form 3'd5,
+    assign pll_bypass = 1'b0;
+    assign pll_clk_selection = 1'b0;
+
+	// Synchronize reset
+	logic reset;
+	logic [3:0] reset_shift = 4'b0; // initial value upon config is reset
+	always_ff @(posedge clk)
+		{ reset, reset_shift[3:0] } <= { !reset_shift[3], reset_shift[2:0], resetn };
+
+	// create ADC inverted clk for output
+	assign ad_sclk  = !clk;
+
+	forge_launcher _chip (
+		// System
+		.clk			( clk ),
+		.reset			( reset ),
+		// Front Panel
+		.fire_button 	( fire_button ),
+		.arm_button 	( arm_button ),
+		.arm_led_n 		( arm_led_n ),
+		.cont_led_n 	( cont_led_n ),
+		.speaker 		( speaker ),
+		// High Voltage
+		.lt3420_charge 	( lt3420_charge ),
+		.lt3420_done   	( lt3420_done   ),
+		.pwm           	( pwm ),
+		.dump			( dump ),
+		// ADC interface
+		.ad_cs			( ad_cs ),
+		.ad_sdata_a 	( ad_sdata_a ),
+		.ad_sdata_b 	( ad_sdata_b ),
+		// Tie off Debug inputs
+		.iset			( 3'b111 ),
+		.key			( 5'b00000 )
+    );
+endmodule // forge_launcher_wrapper 
+		
+		
+		
