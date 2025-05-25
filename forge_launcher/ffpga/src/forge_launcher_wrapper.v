724174a750323ddacf0520f1f283c37e48f01c8d
Eric Pearson <ericp@untether.ai>
2025-05-25 13:28:24 -0400

    Explicit use of LAC (logic as clock block) to get ~clk for the Adc.

--- forge_launcher/ffpga/src/forge_launcher_wrapper.v
+++ forge_launcher/ffpga/src/forge_launcher_wrapper.v
@@ -2,9 +2,11 @@
 (* top *) module forge_launcher_wrapper
 (
 // Forge FPGA built in clk reset
-(* iopad_external_pin, clkbuf_inhibit *) input logic clk,
-(* iopad_external_pin *) input logic resetn,
-
+(* iopad_external_pin, clkbuf_inhibit *) input logic clk2x, // 2x from PLL
+(* iopad_external_pin, clkbuf_inhibit *) input logic clk,   // from LAC 0
+(* iopad_external_pin *) 	output clk_toggle, 					// toggle reg = clk output to LAC
+(* iopad_external_pin *) 	output logic_as_clk0_en,			// enable Logic as clock
+(* iopad_external_pin *) 	input  logic resetn,
 
 	// Input Buttons
 (* iopad_external_pin *)	input  logic arm_button,
@@ -23,10 +25,11 @@
 	
 	// External A/D Converters (2.5v)
 (* iopad_external_pin *)	output logic        ad_cs,
-(* iopad_external_pin *)	output logic		ad_sclk,
+(* iopad_external_pin *)	output logic		ad_sclk, // ~clk, not used internally
 (* iopad_external_pin *)	input  logic  [1:0] ad_sdata_a,
 (* iopad_external_pin *)	input  logic  [1:0] ad_sdata_b,
-	
+
+
 	// Forge PLL control
 (* iopad_external_pin *) output pll_en,
 (* iopad_external_pin *) output [5:0] pll_refdiv,
@@ -38,31 +41,39 @@
 
 );
 
-    // PLL Control, 48 Mhz
+    // PLL Control, 2 x 48 Mhz = 96 Mhz
     assign pll_en = 1'b1;
-    assign pll_refdiv = 6'b00_0001;		// Equivalent value in decimal form 6'd1,
-    assign pll_fbdiv = 12'b0000_0001_1000;	// Equivalent value in decimal form 12'd24,
+    assign pll_refdiv = 6'b00_0101;		// Equivalent value in decimal form 6'd5,
+    assign pll_fbdiv = 12'b0000_1001_0000;	// Equivalent value in decimal form 12'd144,
     assign pll_postdiv1 = 3'b101;		// Equivalent value in decimal form 3'd5,
-    assign pll_postdiv2 = 3'b101;		// Equivalent value in decimal form 3'd5,
+    assign pll_postdiv2 = 3'b011;		// Equivalent value in decimal form 3'd3,
     assign pll_bypass = 1'b0;
     assign pll_clk_selection = 1'b0;
+    
+    // Enable LAC 0
+    assign logic_as_clk0_en = 1'b1;
 
+	// 96 Mhz flops create 48mhz clk and ad_sclk = !clk;
+	logic toggle;
+	logic clk_toggle;
+	logic sclk_toggle;
+	always_ff @(posedge clk2x) begin 
+		toggle <= !toggle;
+		clk_toggle 	<= toggle; // To embend in the IOB FF for REF_LAC_0
+		sclk_toggle <= toggle; // before inversion/phase delay 
+		ad_sclk 	<= sclk_toggle; // inverted to embed in the IOB FF for ad_sclk
+	end
+	
 	// Synchronize reset
 	logic reset;
 	logic [3:0] reset_shift = 4'b0; // initial value upon config is reset
 	always_ff @(posedge clk)
 		{ reset, reset_shift[3:0] } <= { !reset_shift[3], reset_shift[2:0], resetn };
 
-	// create ADC inverted clk for output
-	logic ad_sclk_en;
-	always_ff @(posedge clk)
-		ad_sclk_en <= resetn;
-	assign ad_sclk  = (!clk & ad_sclk);
-
 	forge_launcher _chip (
 		// System
 		.clk			( clk ),
-		.reset			( reset ),
+		.reset			( 1'b0 ), // fpga starts as configured?!!?
 		// Front Panel
 		.fire_button 	( fire_button ),
 		.arm_button 	( arm_button ),
@@ -79,7 +90,7 @@
 		.ad_sdata_a 	( ad_sdata_a ),
 		.ad_sdata_b 	( ad_sdata_b ),
 		// Tie off Debug inputs
-		.iset			( 3'b111 ),
+		.iset			( 3'b111 ), // { !autorun, use_est , mute }
 		.key			( 5'b00000 )
     );
 endmodule // forge_launcher_wrapper 
