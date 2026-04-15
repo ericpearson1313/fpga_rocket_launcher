bb22ee6ab457eb6e753c019653d90da44429524e
DESKTOP-2TS9UB7\ecp <ecp@mgl.ca>
2026-04-15 15:57:04 -0400

    fixed top level (unsimulated!) adc input mapping

--- forge_launcher/ffpga/src/forge_launcher_wrapper.v
+++ forge_launcher/ffpga/src/forge_launcher_wrapper.v
@@ -131,9 +131,9 @@
 		.dump			( dump ),
 		// ADC interface
 		.ad_cs			( ad_cs ),
-		.ad_s_iout		( ad_sdata_a[0] ),
-		.ad_s_vout		( ad_sdata_b[1] ),
-		.ad_s_vcap		( ad_sdata_a[1] ),
+		.ad_s_iout		( ad_sdata_a[1] ),
+		.ad_s_vout		( ad_sdata_a[0] ),
+		.ad_s_vcap		( ad_sdata_b[1] ),
 		.neg_iout		( 1'b0 ),
 		.neg_vout		( 1'b0 ),
 		.neg_vcap		( 1'b0 ),
