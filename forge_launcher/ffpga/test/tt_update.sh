a149a9c53229e5cfd83358a7de3bbc710856523c
Default Application User <ecp@mgl.ca>
2026-05-05 14:41:25 +0200

    script to copy into TIny Tapeout repo

---
+++ forge_launcher/ffpga/test/tt_update.sh
@@ -0,0 +1,12 @@
+# Copy tt files from ffpga to tt directory for tapeout
+git rev-parse HEAD | head -c7 > src_commit.txt
+cp src_commit.txt /foss/designs/tt_eric_launch/.
+cp Makefile  /foss/designs/tt_eric_launch/test/.
+cp README.md  /foss/designs/tt_eric_launch/test/.
+cp requirements.txt  /foss/designs/tt_eric_launch/test/.
+cp tb.gtkw	/foss/designs/tt_eric_launch/test/.
+cp tb.v  /foss/designs/tt_eric_launch/test/.
+cp test.py  /foss/designs/tt_eric_launch/test/.
+cp forge_launcher.sv /foss/designs/tt_eric_launch/src/.
+cp project.v /foss/designs/tt_eric_launch/src/.
+cp lcc_syssim.sv   /foss/designs/tt_eric_launch/src/.
